cffdb62f2914ff4f7eb395df84616bfc15bb2e29
sudasf <1518223884@qq.com>
2016-09-27 14:32:51 +0800

    change

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    port: '9993',
+    ROUTE_BASE_PATH: '/applet',
+
+    cosAppId: '10043034',
+    cosSecretId: 'AKID8OfQbLYczk8w11pE2F7SOyxQVtscPzja',
+    cosSecretKey: '6dfydLEarCLlO42fVJcM56ZzReDb3HkV',
+    cosFileBucket: 'image',
+};
